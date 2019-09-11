de7a5987bb09670ef76694e24b73c4bb3d711627
Erick <erickcfuga@outlook.com>
2019-09-11 16:21:00 -0300

    Add relationships to user model

--- app/User.php
+++ app/User.php
@@ -8,32 +8,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    public function wallet()
+    {
+	return $this->hasOne('Wallet');
+    }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function monthlyBudgets()
+    {
+	return $this->hasMany('MonthlyBudget');
+    }
 }
+
+:q
