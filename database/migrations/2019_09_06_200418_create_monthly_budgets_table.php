e5e3217c0bd4d34b4cc131436a8ab138aad3b5bf
Erick <erickcfuga@outlook.com>
2019-09-11 16:19:56 -0300

    Add monthly budget migration

---
+++ database/migrations/2019_09_06_200418_create_monthly_budgets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMonthlyBudgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monthly_budgets', function (Blueprint $table) {
+	    $table->bigIncrements('id');
+	    $table->bigInteger('starting_balance')->default(0);
+	    $table->bigInteger('end_balance')->default(0);
+	    $table->bigInteger('expected_incomings')->default(0);
+	    $table->bigInteger('expected_expenses')->default(0);
+	    $table->string('month', 16)->default(date('F'));
+	    $table->string('year', 4)->default(date('Y'));
+	    $table->unsignedBigInteger('user_id');
+	    $table->foreign('user_id')
+		  ->references('id')
+	  	  ->on('users')
+	  	  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monthly_budgets');
+    }
+}
