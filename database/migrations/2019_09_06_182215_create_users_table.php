4636f6d056c513484ccc62861b4864181327df77
Erick <erickcfuga@outlook.com>
2019-09-11 16:20:44 -0300

    Fix timestamps

--- database/migrations/2019_09_06_182215_create_users_table.php
+++ database/migrations/2019_09_06_182215_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFailedJobsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+	    $table->string('username', 32)->unique();
+	    $table->string('email', 64)->unique();
+            $table->timestamps();
         });
     }
 
@@ -30,6 +28,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('users');
     }
 }
--- database/migrations/2019_09_10_183329_create_wallets_table.php
+++ database/migrations/2019_09_10_183329_create_wallets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateWalletsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wallets', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+	    $table->bigInteger('current_balance')->default(0);
+	    $table->string('currency', 3); # USD, BRL, etc...
             $table->timestamps();
+	    $table->unsignedBigInteger('user_id');
+	    $table->foreign('user_id')
+		  ->references('id')
+	  	  ->on('users')
+	  	  ->onDelete('cascade');
         });
     }
 
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('wallets');
     }
 }
