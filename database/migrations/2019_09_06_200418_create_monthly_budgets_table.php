f0c6d6fa4196baf754b8f4fa74e0a005b2af2e31
Erick Fuga <erick_fuga@hotmail.com>
2019-10-11 14:54:52 -0300

    Fix migrations

--- database/migrations/2019_09_06_200418_create_monthly_budgets_table.php
+++ database/migrations/2019_09_06_200418_create_monthly_budgets_table.php
@@ -17,8 +17,8 @@ class CreateMonthlyBudgetsTable extends Migration
 	    $table->bigIncrements('id');
 	    $table->bigInteger('starting_balance')->default(0);
 	    $table->bigInteger('end_balance')->default(0);
-	    $table->bigInteger('expected_incomings')->default(0);
-	    $table->bigInteger('expected_expenses')->default(0);
+	    $table->unsignedBigInteger('expected_incomings')->default(0);
+	    $table->unsignedBigInteger('expected_expenses')->default(0);
 	    $table->string('month', 16)->default(date('F'));
 	    $table->string('year', 4)->default(date('Y'));
 	    $table->unsignedBigInteger('user_id');
--- database/migrations/2019_09_10_182050_create_transactions_table.php
+++ database/migrations/2019_09_10_182050_create_transactions_table.php
@@ -21,7 +21,7 @@ class CreateTransactionsTable extends Migration
 	    $table->string('description', 128); # This is what the transaction repersent
 	    $table->string('category', 32)->index(); # The category the transacion is in (e.i. heath, food, home')
 	    $table->string('channel', 32)->index(); # The channel in which the transaction was generated (i.e. a credit card)
-	    $table->string('status', 16)->default('not paid')->index(); # Status of the transaction (i.e. paid, not paid, received)
+	    $table->string('status', 16)->default('not_paid')->index(); # Status of the transaction (i.e. paid, not paid, received)
 	    $table->unsignedBigInteger('monthly_budget_id');
 	    $table->foreign('monthly_budget_id')
 		  ->references('id')
